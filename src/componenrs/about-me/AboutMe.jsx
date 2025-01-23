a721f3864d859d8027502284cd9c410d7ab3d5ef
Muxsimov <muxsimovasadbek665@gmail.com>
2025-01-23 10:33:00 +0500

    finish

--- src/componenrs/about-me/AboutMe.jsx
+++ src/componenrs/about-me/AboutMe.jsx
@@ -5,13 +5,13 @@ function AboutMe() {
     return (
         <>
             <nav className='p-5 flex justify-center text-white w-[100%] max-w-[1440px]'>
-                <ul className='flex gap-28 '>
+                <ul className='flex gap-20 md:gap-28 '>
                     <Link to='/'><a href="">Main</a></Link>
                     <Link to='/AboutMe'><a href="">About me</a></Link>
                     <Link to='/Contact'><a href="">Contact</a></Link>
                 </ul>
             </nav>
-            <div className="bagraund max-w-[1440px] text-white p-10 h-[660px] md:h-[660px]">
+            <div className="bagraund max-w-[1440px] text-white p-10 h-[600px] md:h-[660px]">
                 <div className='flex flex-col items-center'>
                     <h1 className='text-[22px] mb-5'>Men haqimda</h1>
                     <p className='text-[22px] mb-5'>
@@ -27,7 +27,7 @@ function AboutMe() {
                         Najot Ta'lim o'quv markazida frontend dasturchi bo‘lib tahsil olyapman.
                     </p>
                 </div>
-                <div className='flex justify-end items-end h-[55%]'>
+                <div className='flex justify-end items-end h-[38%] md:h-[55%]'>
                     <button className="bg-white text-black px-[10px] py-[5px] rounded-md text-[20px]" onClick={() => window.history.back()}>
                         Back
                     </button>
